aa06b05594e827d8ab6b6dcc86b3115b2e8f6e45
Asif Shikder <shikdernyc@gmail.com>
2021-04-05 23:01:34 -0400

    Added placeholder components

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,6 +50,7 @@ module.exports = {
       }
    ],
     "jsx-a11y/no-static-element-interactions": "off",
-    "jsx-a11y/no-noninteractive-element-interactions": "off"
+    "jsx-a11y/no-noninteractive-element-interactions": "off",
+    "@typescript-eslint/no-unused-vars": [1, { "argsIgnorePattern": "^_" }]
   },
 };
---
+++ src/components/animal_card/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AnimalCard: React.FC = () => {
+  return <h1>Hello Animal Card</h1>;
+};
+
+export default AnimalCard;
